27040a9a61329266c9e5457b51b8dec05ec47286
asingh20-tech <asingh20@oswego.edu>
2026-04-16 21:32:20 -0400

    made a 2d array

--- cachelab/cachelab.c
+++ cachelab/cachelab.c
@@ -9,58 +9,75 @@
 #include <unistd.h>
 
 // more libraries if needed for your program
-struct cacheline{
+struct cacheline
+{
     long long tag;
     int lru;
     int valid;
 };
 
 // print result of cache simulation showing hit number, miss number, miss rate, and total running time
-void printResult(int hits, int misses, int missRate, int runTime) {
-    printf("[result] hits: %d misses: %d miss rate: %d%% total running time: %d cycle\n",hits, misses, missRate, runTime);
+void printResult(int hits, int misses, int missRate, int runTime)
+{
+    printf("[result] hits: %d misses: %d miss rate: %d%% total running time: %d cycle\n", hits, misses, missRate, runTime);
 }
 
 // main function should be coded here
 
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
     long long m;
     int s;
     int e;
     int b;
-    int S;
-    int E;
+
     int hits = 0;
     int misses = 0;
-    char filename[100]; 
+    char filename[100];
     char replacement[50];
     int opt;
 
-    while( (opt = getopt(argc, argv, "m:s:e:b:i:r:")) != -1){
-        switch(opt){
-            case 'm':
-                m = atoi(optarg);
-                break;
-            case 's':
-                s = atoi(optarg);
-                break;
-            case 'e':
-                e = atoi(optarg);
-                break;
-            case 'b':
-                b = atoi(optarg);
-                break;
-            case 'i':
-                strcpy(filename,optarg);  
-                break;
-            case 'r':
-                strcpy(replacement,optarg);
-                break;                  
+    while ((opt = getopt(argc, argv, "m:s:e:b:i:r:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'm':
+            m = atoi(optarg);
+            break;
+        case 's':
+            s = atoi(optarg);
+            break;
+        case 'e':
+            e = atoi(optarg);
+            break;
+        case 'b':
+            b = atoi(optarg);
+            break;
+        case 'i':
+            strcpy(filename, optarg);
+            break;
+        case 'r':
+            strcpy(replacement, optarg);
+            break;
         }
-
-
     }
-    printf("%lld m , %d s , %d e , %d b , %s i , %s r\n", m, s, e, b, filename, replacement);
-    
+    int S = 1 << s;
+    int E = 1 << e;
+    printf("%lld m , %d s , %d e , %d b , %s i , %s r\n ", m, s, e, b, filename, replacement);
+    struct cacheline **cache = malloc(S * sizeof(struct cacheline *));
+
+    for (int i = 0; i < S; i++)
+    {
+        cache[i] = malloc(E * sizeof(struct cacheline));
+        for (int j = 0; j < E; j++)
+        {
+            cache[i][j].valid = 0;
+            cache[i][j].tag = 0;
+            cache[i][j].lru = 0;
+        }
+    }
+    printf("S = %d , E = %d", S, E);
+
     return 0;
 }
 
