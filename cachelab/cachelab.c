6f1d1e0bf9508e9ebea4ebc2991d564480b14b5f
asingh20-tech <asingh20@oswego.edu>
2026-04-16 20:57:38 -0400

    getopt check

---
+++ cachelab/cachelab.c
@@ -0,0 +1,67 @@
+//
+// cachelab.c - Cache lab template
+//
+//
+#include "cachelab.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+// more libraries if needed for your program
+struct cacheline{
+    long long tag;
+    int lru;
+    int valid;
+};
+
+// print result of cache simulation showing hit number, miss number, miss rate, and total running time
+void printResult(int hits, int misses, int missRate, int runTime) {
+    printf("[result] hits: %d misses: %d miss rate: %d%% total running time: %d cycle\n",hits, misses, missRate, runTime);
+}
+
+// main function should be coded here
+
+int main(int argc, char** argv){
+    long long m;
+    int s;
+    int e;
+    int b;
+    int S;
+    int E;
+    int hits = 0;
+    int misses = 0;
+    char filename[100]; 
+    char replacement[50];
+    int opt;
+
+    while( (opt = getopt(argc, argv, "m:s:e:b:i:r:")) != -1){
+        switch(opt){
+            case 'm':
+                m = atoi(optarg);
+                break;
+            case 's':
+                s = atoi(optarg);
+                break;
+            case 'e':
+                e = atoi(optarg);
+                break;
+            case 'b':
+                b = atoi(optarg);
+                break;
+            case 'i':
+                strcpy(filename,optarg);  
+                break;
+            case 'r':
+                strcpy(replacement,optarg);
+                break;                  
+        }
+
+
+    }
+    printf("%lld m , %d s , %d e , %d b , %s i , %s r\n", m, s, e, b, filename, replacement);
+    
+    return 0;
+}
+
+// GOOD LUCK!
